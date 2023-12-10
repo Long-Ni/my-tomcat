a8b700b432832dc3c8ae567c988043673d48acc5
Jilong Ni <ni@192.168.0.106>
2023-12-10 16:52:34 +0800

    http-session-support

---
+++ http-session-support/src/main/java/org/example/engine/HttpServletRequestImpl.java
@@ -0,0 +1,443 @@
+package org.example.engine;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import org.example.engine.support.HttpHeaders;
+import org.example.engine.support.Parameters;
+import org.example.exchange.HttpExchangeRequest;
+import org.example.utils.HttpUtils;
+
+import java.io.*;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.security.Principal;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class HttpServletRequestImpl implements HttpServletRequest {
+
+    final ServletContextImpl servletContext;
+    final HttpServletResponse response;
+    final HttpHeaders headers;
+    final Parameters parameters;
+    final HttpExchangeRequest httpExchangeRequest;
+
+    Boolean inputCalled = null;
+
+
+    public HttpServletRequestImpl(HttpExchangeRequest httpExchangeRequest, ServletContextImpl servletContext, HttpServletResponse response) {
+        this.httpExchangeRequest = httpExchangeRequest;
+        this.headers = new HttpHeaders(httpExchangeRequest.getRequestHeaders());
+        this.parameters = new Parameters(httpExchangeRequest, StandardCharsets.UTF_8);
+        this.servletContext = servletContext;
+        this.response = response;
+    }
+
+    @Override
+    public String getMethod() {
+        return httpExchangeRequest.getRequestMethod();
+    }
+
+    @Override
+    public String getQueryString() {
+        return httpExchangeRequest.getRequestURI().getQuery();
+    }
+
+    @Override
+    public String getRequestURI() {
+        return httpExchangeRequest.getRequestURI().toString();
+    }
+
+    @Override
+    public String getServletPath() {
+        return httpExchangeRequest.getRequestURI().getPath();
+    }
+
+    @Override
+    public String getAuthType() {
+        return null;
+    }
+
+    @Override
+    public String getParameter(String name) {
+        return this.parameters.getParameter(name);
+    }
+
+    @Override
+    public Enumeration<String> getParameterNames() {
+        return this.parameters.getParameterNames();
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        return this.parameters.getParameterValues(name);
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+        return this.parameters.getParameterMap();
+    }
+
+    Map<String, String> parseQuery(String query) {
+        if (query == null || query.isEmpty()) {
+            return Map.of();
+        }
+        String[] ss = Pattern.compile("\\&").split(query);
+        Map<String, String> map = new HashMap<>();
+        for (String s : ss) {
+            int n = s.indexOf('=');
+            if (n >= 1) {
+                String key = s.substring(0, n);
+                String value = s.substring(n + 1);
+                map.putIfAbsent(key, URLDecoder.decode(value, StandardCharsets.UTF_8));
+            }
+        }
+        return map;
+    }
+
+
+    @Override
+    public HttpSession getSession(boolean create) {
+        String sessionId = null;
+        Cookie[] cookies = getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("JSESSIONID".equals(cookie.getName())) {
+                    sessionId = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        if (sessionId == null && !create) {
+            return null;
+        }
+        if (sessionId == null) {
+            if (this.response.isCommitted()) {
+                throw new IllegalStateException("Cannot create session for response is commited.");
+            }
+            sessionId = UUID.randomUUID().toString();
+            // set cookie:
+            String cookieValue = "JSESSIONID=" + sessionId + "; Path=/; SameSite=Strict; HttpOnly";
+            this.response.addHeader("Set-Cookie", cookieValue);
+        }
+        return this.servletContext.sessionManager.getSession(sessionId);
+    }
+
+    @Override
+    public HttpSession getSession() {
+        return getSession(true);
+    }
+
+    @Override
+    public String changeSessionId() {
+        throw new UnsupportedOperationException("changeSessionId() is not supported.");
+    }
+
+    @Override
+    public boolean isRequestedSessionIdValid() {
+        return false;
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromCookie() {
+        return true;
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromURL() {
+        return false;
+    }
+
+    @Override
+    public boolean authenticate(HttpServletResponse httpServletResponse) throws IOException, ServletException {
+        return false;
+    }
+
+    @Override
+    public Cookie[] getCookies() {
+        String cookieValue = this.getHeader("Cookie");
+        return HttpUtils.parseCookies(cookieValue);
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        if (this.inputCalled == null) {
+            this.inputCalled = Boolean.TRUE;
+            return new ServletInputStreamImpl(this.httpExchangeRequest.getRequestBody());
+        }
+        throw new IllegalStateException("Cannot reopen input stream after " + (this.inputCalled ? "getInputStream()" : "getReader()") + " was called.");
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        if (this.inputCalled == null) {
+            this.inputCalled = Boolean.FALSE;
+            return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(this.httpExchangeRequest.getRequestBody()), StandardCharsets.UTF_8));
+        }
+        throw new IllegalStateException("Cannot reopen input stream after " + (this.inputCalled ? "getInputStream()" : "getReader()") + " was called.");
+    }
+
+    // header operations //////////////////////////////////////////////////////
+
+    @Override
+    public long getDateHeader(String name) {
+        return this.headers.getDateHeader(name);
+    }
+
+    @Override
+    public String getHeader(String name) {
+        return this.headers.getHeader(name);
+    }
+
+    @Override
+    public Enumeration<String> getHeaders(String name) {
+        List<String> hs = this.headers.getHeaders(name);
+        if (hs == null) {
+            return Collections.emptyEnumeration();
+        }
+        return Collections.enumeration(hs);
+    }
+
+    @Override
+    public Enumeration<String> getHeaderNames() {
+        return Collections.enumeration(this.headers.getHeaderNames());
+    }
+
+    @Override
+    public int getIntHeader(String name) {
+        return this.headers.getIntHeader(name);
+    }
+
+    // not implemented yet:
+
+    @Override
+    public String getPathInfo() {
+        return null;
+    }
+
+    @Override
+    public String getPathTranslated() {
+        return null;
+    }
+
+    @Override
+    public String getContextPath() {
+        return null;
+    }
+
+    @Override
+    public String getRemoteUser() {
+        return null;
+    }
+
+    @Override
+    public boolean isUserInRole(String s) {
+        return false;
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+        return null;
+    }
+
+    @Override
+    public String getRequestedSessionId() {
+        return null;
+    }
+
+    @Override
+    public StringBuffer getRequestURL() {
+        return null;
+    }
+
+    @Override
+    public void login(String s, String s1) throws ServletException {
+
+    }
+
+    @Override
+    public void logout() throws ServletException {
+
+    }
+
+    @Override
+    public Collection<Part> getParts() throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public Part getPart(String s) throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public <T extends HttpUpgradeHandler> T upgrade(Class<T> aClass) throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public Object getAttribute(String s) {
+        return null;
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+        return null;
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return null;
+    }
+
+    @Override
+    public void setCharacterEncoding(String s) throws UnsupportedEncodingException {
+
+    }
+
+    @Override
+    public int getContentLength() {
+        return 0;
+    }
+
+    @Override
+    public long getContentLengthLong() {
+        return 0;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public String getProtocol() {
+        return null;
+    }
+
+    @Override
+    public String getScheme() {
+        return null;
+    }
+
+    @Override
+    public String getServerName() {
+        return null;
+    }
+
+    @Override
+    public int getServerPort() {
+        return 0;
+    }
+
+    @Override
+    public String getRemoteAddr() {
+        return null;
+    }
+
+    @Override
+    public String getRemoteHost() {
+        return null;
+    }
+
+    @Override
+    public void setAttribute(String s, Object o) {
+
+    }
+
+    @Override
+    public void removeAttribute(String s) {
+
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+
+    @Override
+    public Enumeration<Locale> getLocales() {
+        return null;
+    }
+
+    @Override
+    public boolean isSecure() {
+        return false;
+    }
+
+    @Override
+    public RequestDispatcher getRequestDispatcher(String s) {
+        return null;
+    }
+
+    @Override
+    public int getRemotePort() {
+        return 0;
+    }
+
+    @Override
+    public String getLocalName() {
+        return null;
+    }
+
+    @Override
+    public String getLocalAddr() {
+        return null;
+    }
+
+    @Override
+    public int getLocalPort() {
+        return 0;
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return null;
+    }
+
+    @Override
+    public AsyncContext startAsync() throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public boolean isAsyncStarted() {
+        return false;
+    }
+
+    @Override
+    public boolean isAsyncSupported() {
+        return false;
+    }
+
+    @Override
+    public AsyncContext getAsyncContext() {
+        return null;
+    }
+
+    @Override
+    public DispatcherType getDispatcherType() {
+        return null;
+    }
+
+    @Override
+    public String getRequestId() {
+        return null;
+    }
+
+    @Override
+    public String getProtocolRequestId() {
+        return null;
+    }
+
+    @Override
+    public ServletConnection getServletConnection() {
+        return null;
+    }
+}
---
+++ http-session-support/src/main/java/org/example/exchange/HttpConnector.java
@@ -0,0 +1,67 @@
+package org.example.exchange;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.example.engine.HttpServletRequestImpl;
+import org.example.engine.HttpServletResponseImpl;
+import org.example.engine.ServletContextImpl;
+import org.example.engine.filter.HelloFilter;
+import org.example.engine.filter.LogFilter;
+import org.example.engine.servlet.HelloServlet;
+import org.example.engine.servlet.IndexServlet;
+import org.example.engine.servlet.LoginServlet;
+import org.example.engine.servlet.LogoutServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.time.Duration;
+import java.util.List;
+
+public class HttpConnector implements HttpHandler, AutoCloseable {
+
+    final Logger logger = LoggerFactory.getLogger(getClass());
+
+    final HttpServer httpServer;
+    final ServletContextImpl servletContext;
+    final Duration stopDelay = Duration.ofSeconds(5);
+
+    public HttpConnector() throws IOException {
+        this.servletContext = new ServletContextImpl();
+        this.servletContext.initServlets(List.of(IndexServlet.class, HelloServlet.class, LoginServlet.class, LogoutServlet.class));
+        this.servletContext.initFilters(List.of(LogFilter.class, HelloFilter.class));
+
+        // start http server:
+        String host = "0.0.0.0";
+        int port = 8080;
+
+        this.httpServer = HttpServer.create(new InetSocketAddress(host, port), 0, "/", this);
+        this.httpServer.start();
+        logger.info("start my-http-server at {}:{}", host, port);
+        logger.info("please access: http://localhost:8080");
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) {
+
+        logger.info("{}: {}?{}", exchange.getRequestMethod(), exchange.getRequestURI().getPath(), exchange.getRequestURI().getRawQuery());
+
+        var adapter = new HttpExchangeAdapter(exchange);
+        var responseAdapter = new HttpServletResponseImpl(adapter);
+        var requestAdapter = new HttpServletRequestImpl(adapter, this.servletContext, responseAdapter);
+
+        try {
+            this.servletContext.process(requestAdapter, responseAdapter);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.httpServer.stop((int) this.stopDelay.toSeconds());
+    }
+}
